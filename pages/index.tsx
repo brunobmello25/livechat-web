60d84b5f3195e4a20fbcbbb7c1f82272470c8308
Bruno Mello <bruno.barros.mello@gmail.com>
2022-07-02 18:01:07 -0300

    finish chat style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
-import { ReactElement, useEffect, useRef, useState } from "react";
+import { FormEvent, ReactElement, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import { FaPaperPlane } from "react-icons/fa";
 
 import { Loading } from "../components/Loading";
 import { useUser, User } from "../hooks/user";
@@ -8,7 +9,7 @@ import { useUser, User } from "../hooks/user";
 type MessageType = {
   id: string;
   user: User;
-  message: string;
+  data: string;
 };
 
 export default function ChatPage(): ReactElement {
@@ -17,6 +18,7 @@ export default function ChatPage(): ReactElement {
   const router = useRouter();
 
   const [messages] = useState<MessageType[]>([]);
+  const [message, setMessage] = useState("");
 
   useEffect(() => {
     if (messagesContainerRef.current) {
@@ -25,26 +27,52 @@ export default function ChatPage(): ReactElement {
     }
   }, []);
 
+  useEffect(() => {
+    if (!user.user && !user.loading) {
+      router.push("/login");
+    }
+  }, [router, user.loading, user.user]);
+
+  async function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log(message);
+  }
+
   if (user.loading) {
     return <Loading />;
   }
 
-  if (!user.user) {
-    router.push("/login");
-  }
-
   return (
     <Container>
-      <MessagesContainer ref={messagesContainerRef}>
-        {messages.map((message) => (
-          <Message key={message.id}>
-            <strong>{message.user.name}</strong>
-            <p>{message.message}</p>
-          </Message>
-        ))}
-      </MessagesContainer>
+      {messages.length > 0 ? (
+        <MessagesContainer ref={messagesContainerRef}>
+          {messages.map((message) => (
+            <Message key={message.id}>
+              <strong>{message.user.name}</strong>
+              <p>{message.data}</p>
+            </Message>
+          ))}
+        </MessagesContainer>
+      ) : (
+        <EmptyMessagesContainer>
+          <p>Ops! Parece que não tem ninguém aqui</p>
+        </EmptyMessagesContainer>
+      )}
 
-      <Form></Form>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Digite aqui sua mensagem"
+        />
+
+        <button disabled={!message} type="submit">
+          <FaPaperPlane />
+        </button>
+      </Form>
     </Container>
   );
 }
@@ -59,6 +87,20 @@ const Container = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: stretch;
+`;
+
+const EmptyMessagesContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  p {
+    font-size: 1.6em;
+    font-family: "Roboto", sans-serif;
+    text-align: center;
+    color: ${(props) => props.theme.colors.onSurface};
+  }
 `;
 
 const MessagesContainer = styled.div`
@@ -98,4 +140,39 @@ const Message = styled.div`
 const Form = styled.form`
   background-color: ${(props) => props.theme.colors.surface};
   min-height: 20px;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
+  border-radius: 5px;
+
+  input {
+    flex: 1;
+    background: transparent;
+    border: none;
+    padding: 10px;
+    font-size: 1.2em;
+    font-family: "Roboto", sans-serif;
+    color: ${(props) => props.theme.colors.onSurface};
+    border-radius: 5px;
+  }
+
+  button {
+    padding: 5px 15px;
+    margin-left: 5px;
+    font-size: 1em;
+    font-weight: bold;
+    color: white;
+    background: ${(props) => props.theme.colors.primary};
+    border: none;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    &:disabled {
+      opacity: 0.7;
+    }
+  }
 `;
